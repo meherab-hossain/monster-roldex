7e8407db8fd3b531465eb9f71819b2dee5f2fa73
Md. Meherab Hossain <38518474+meherab-hossain@users.noreply.github.com>
2020-07-15 22:37:55 +0600

    Updates

---
+++ precache-manifest.d7a372add55e7a3cecd7f3b3d9318bdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f36e4f23c586e8ca627241df99b443",
+    "url": "/monster-roldex/index.html"
+  },
+  {
+    "revision": "23ca9a94be84ee9df0f1",
+    "url": "/monster-roldex/static/css/main.10fb8d49.chunk.css"
+  },
+  {
+    "revision": "cb94cf0818e72df96def",
+    "url": "/monster-roldex/static/js/2.adb19f6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-roldex/static/js/2.adb19f6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ca9a94be84ee9df0f1",
+    "url": "/monster-roldex/static/js/main.45dc203d.chunk.js"
+  },
+  {
+    "revision": "7e15469b2f3fa656a7c3",
+    "url": "/monster-roldex/static/js/runtime-main.033a2441.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-roldex/static/media/logo.5d5d9eef.svg"
+  }
+]);
